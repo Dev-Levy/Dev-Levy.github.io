b9a52a228a1123f728a89c6e2d44318f6f93f94b
Oláh Levente <olahlevente1016@gmail.com>
2025-07-15 11:53:00 +0200

    Refactor README and update layout styles for improved responsiveness

--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,8 @@
     import ExportZone from "./lib/ExportZone.svelte";
 </script>
 
+<h1>Sheet Music Maker</h1>
+<h2>Work in progress...</h2>
 <main>
     <UploadsZone />
     <AnalyzeZone />
@@ -11,18 +13,32 @@
 </main>
 
 <style>
+    h2{
+        font-size: 3rem;
+        font-style: italic;
+        margin-bottom: 1em;
+    }
     main {
         display: flex;
         flex-direction: row;
         align-items: center;
         justify-content: space-evenly;
-        width: 1800px;
+        width: 80%;
         height: 600px;
+    }
+
+    @media(max-width: 900px) {
+        main {
+            flex-direction: column;
+            width: 100%;
+            height: auto;
+        }
     }
 
     :global(*) {
         box-sizing: border-box;
         margin: 0;
         padding: 0;
+        /* border: red 1px solid; */
     }
 </style>
