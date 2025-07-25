16d4dc29af1d7c716e4ce1f4dfd75f45ba3621e1
Olah Levente <oll4bp@bosch.com>
2025-07-25 11:14:45 +0200

    a simple GET works

--- src/lib/ExportZone.svelte
+++ src/lib/ExportZone.svelte
@@ -1,55 +1,71 @@
 <script>
-    function download() {
-        console.log("dowloading PDF!");
+  const url = "http://localhost:8080/";
+
+  async function download() {
+    console.log("dowloading PDF!");
+
+    try {
+      const response = await fetch(url.concat("recording/1"), {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const json = await response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error.message);
     }
+  }
 </script>
 
 <div class="ExportZone">
-    <div id="preview">
-        <dir id="PDF_container">
-            <div id="PDF">PDF preview</div>
-        </dir>
-    </div>
-    <div id="download">
-        <button onclick={download}>Download</button>
-    </div>
+  <div id="preview">
+    <dir id="PDF_container">
+      <div id="PDF">PDF preview</div>
+    </dir>
+  </div>
+  <div id="download">
+    <button onclick={download}>Download</button>
+  </div>
 </div>
 
 <style>
-    .ExportZone {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+  .ExportZone {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-        height: 100%;
-        width: 40%;
-        margin: 2em;
-    }
+    height: 100%;
+    width: 40%;
+    margin: 2em;
+  }
 
-    #preview {
-        height: 100%;
-        width: 100%;
-        max-width: 500px;
-    }
-    #PDF_container {
-        padding: 1em;
-        background-color: #415a77;
-        border: 2px #e0e1dd solid;
-        border-radius: 5px;
-        height: 100%;
-    }
-    #PDF {
-        padding: 2em;
-        border: 2px #e0e1dd dashed;
-        border-radius: 5px;
-        background-color: #1b263b;
-        height: 100%;
-    }
+  #preview {
+    height: 100%;
+    width: 100%;
+    max-width: 500px;
+  }
+  #PDF_container {
+    padding: 1em;
+    background-color: #415a77;
+    border: 2px #e0e1dd solid;
+    border-radius: 5px;
+    height: 100%;
+  }
+  #PDF {
+    padding: 2em;
+    border: 2px #e0e1dd dashed;
+    border-radius: 5px;
+    background-color: #1b263b;
+    height: 100%;
+  }
 
-    #download {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin: 2em 0;
-    }
+  #download {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 2em 0;
+  }
 </style>
--- src/lib/UploadsZone.svelte
+++ src/lib/UploadsZone.svelte
@@ -1,74 +1,72 @@
 <script lang="ts">
-    import FileCard from "./FileCard.svelte";
+  import FileCard from "./FileCard.svelte";
 
-    let items = $state([
-        { id: 1, name: "file1" },
-        { id: 2, name: "file2" },
-        { id: 3, name: "file3" },
-    ]);
+  const url = "https://localhost:8080/";
 
-    let file: String = $state("");
-    $inspect("File is updated!", file);
+  let items = $state([
+    { id: 1, name: "file1" },
+    { id: 2, name: "file2" },
+    { id: 3, name: "file3" },
+  ]);
 
-    function deleteItem(id: number) {
-        console.log("deleted item with id: " + id);
+  let file: String = $state("");
+  $inspect("File is updated!", file);
 
-        items = items.filter((item) => item.id !== id);
-        file = "";
-    }
+  function deleteItem(id: number) {
+    console.log("deleted item with id: " + id);
 
-    function upload() {
-        console.log("uploading file");
+    items = items.filter((item) => item.id !== id);
+    file = "";
+  }
 
-        const newId = Math.max(0, ...items.map((i) => i.id)) + 1;
-        items = [...items, { id: newId, name: `file${newId}` }];
-        file = "";
-    }
+  async function upload() {
+    console.log("uploading file");
+  }
 
-    function playItem(id: number) {
-        console.log("playing item with id: " + id);
-    }
+  function playItem(id: number) {
+    console.log("playing item with id: " + id);
+  }
 </script>
 
 <div class="UploadsZone">
-    <div id="list-items">
-        {#each items as item}
-            <FileCard
-                name={item.name}
-                onDelete={() => deleteItem(item.id)}
-                onPlay={() => playItem(item.id)}
-            />
-        {/each}
-    </div>
-    <div id="upload">
-        <input type="file" bind:value={file} />
-        <button onclick={upload}>Upload</button>
-    </div>
+  <div id="list-items">
+    {#each items as item}
+      <FileCard
+        name={item.name}
+        onDelete={() => deleteItem(item.id)}
+        onPlay={() => playItem(item.id)}
+      />
+    {/each}
+  </div>
+  <div id="upload">
+    <input type="file" bind:value={file} />
+    <button onclick={upload}>Upload</button>
+  </div>
 </div>
 
 <style>
-    .UploadsZone {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        height: 100%;
-        width: 40%;
-        margin: 2em;
-    }
-    #list-items {
-        width: 100%;
-        height: 100%;
-        overflow-y: scroll;
-        scrollbar-width: none;
-        max-width: 500px;
+  .UploadsZone {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 100%;
+    width: 40%;
+    margin: 2em;
+  }
+  #list-items {
+    width: 100%;
+    height: 100%;
+    overflow-y: scroll;
+    scrollbar-width: none;
+    max-width: 500px;
 
-        border-radius: 5px;
-        border: 2px #e0e1dd solid;
-    }
-    #upload {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin: 2em 0;
-    }
+    border-radius: 5px;
+    border: 2px #e0e1dd solid;
+  }
+  #upload {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 2em 0;
+  }
 </style>
